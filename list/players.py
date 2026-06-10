0da2b640777b5455124e87433bd85dce652db767
Shreya <shreyasingh85301@gmail.com>
2026-06-10 19:17:58 +0530

    add players.py

---
+++ list/players.py
@@ -0,0 +1,22 @@
+players = ["Mario", "Luigi"]
+print(players)
+
+# to add items to list..
+players.append("Arya")
+print(players)
+
+# to extend list...
+players.extend(["Sansa","Jon","Theon"])
+print(players)
+
+# for removing items..
+players.remove("Sansa")
+print(players)
+
+#for inserting at perticular index..
+players.insert(1,"Sansa")
+print(players)
+
+#for reversing the list
+players.reverse()
+print(players)
